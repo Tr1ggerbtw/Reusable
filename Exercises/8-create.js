0f3398fc3e73ef5de646add1ad52b82698a7c793
Tr1ggerbtw <lehesadanylo27@gmail.com>
2024-12-06 14:56:30 +0200

    done

--- Exercises/8-create.js
+++ Exercises/8-create.js
@@ -5,6 +5,11 @@
   Example: `createUser('Marcus Aurelius', 'Roma')`
   will return object `{ name: 'Marcus Aurelius', city: 'Roma' }` */
 
-const createUser = null;
+const createUser = (name, city) =>  {
+const user = {name: name, city: city};
+console.log(user);
+ }
+
+createUser('Daniil', 'Kyiv');
 
 module.exports = { createUser };
