dc1b51fe11f733d1d59637f5cf5c295fa68387ca
Tr1ggerbtw <lehesadanylo27@gmail.com>
2024-12-06 04:26:33 +0200

    Update 4-range.js

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -3,6 +3,15 @@
 // Implement function `range(start: number, end: number): array` returning
 // array with all numbers from the range [15, 30] including endpoints
 
-const range = null;
-
+const range = (start, end) => {
+    const massive = []
+  
+    for (let i = start; i <= end; i++) {
+      massive.push(i)
+    }
+  
+    return massive
+  }
+  
+  console.dir(range(15, 30))
 module.exports = { range };
